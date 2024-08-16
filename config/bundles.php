24392221f14d52b454927cdcfa134e75ac4becab
Hasina <hasinasayan@gmail.com>
2024-08-16 11:52:27 +0300

    add webp

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     VictorPrdh\RecaptchaBundle\RecaptchaBundle::class => ['all' => true],
+    CodeBuds\WebPConversionBundle\WebPConversionBundle::class => ['all' => true],
 ];
